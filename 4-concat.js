129ab12e95d17af3d275124f4c0c59fe90dae092
hazem <hazemasaidui@gmail.com>
2025-09-14 16:36:09 +0300

    edit file

--- 4-concat.js
+++ 4-concat.js
@@ -3,4 +3,4 @@ const Hazem = process.argv[2];
 const Ahmed = process.argv[3];
 
 console.log (`${Hazem} is ${Ahmed}`)
-console.log (Hazem + " " + "is" + " " + Ahmed)
+// console.log (Hazem + " " + "is" + " " + Ahmed)
