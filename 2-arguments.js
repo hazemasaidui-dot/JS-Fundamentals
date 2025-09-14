9ef4da3faf757a8cf8ac88de4afb76ba63a98095
hazem <hazemasaidui@gmail.com>
2025-09-14 14:55:12 +0300

    task 2 updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,11 @@
 function hasArguments (...args){
-    if(args.length === 1){
-        console.log("Argument found")
-    } else if(args.length > 1){
-        console.log("Arguments found")
-    } else {
+    if(!args || args.length===0){
         console.log("No argument")
+    } else {
+        if(args.length > 1){
+        console.log("Arguments found")
+        } else {
+        console.log("Argument found")
+        }
     }
 }
-
