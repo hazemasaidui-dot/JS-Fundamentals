452a3c9fdb8761ab8d8c855b53c94c6408585a19
hazem <hazemasaidui@gmail.com>
2025-09-14 16:32:03 +0300

    test 4

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+const { argv } = require('node:process');
+const Hazem = process.argv[2];
+const Ahmed = process.argv[3];
+
+console.log (`${Hazem} is ${Ahmed}`)
+console.log (Hazem + " " + "is" + " " + Ahmed)
