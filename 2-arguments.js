94b44801c1ea384050cb4f17c8b7121418765a39
hazem <hazemasaidui@gmail.com>
2025-09-14 14:55:12 +0300

    task 2 updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,25 @@
-function hasArguments (...args){
-    if(args.length === 1){
-        console.log("Argument found")
-    } else if(args.length > 1){
-        console.log("Arguments found")
-    } else {
-        console.log("No argument")
-    }
-}
+// function hasArguments (...args){
+//     if(!args || args.length===0){
+//         console.log("No argument")
+//     } else {
+//         if(args.length > 1){
+//         console.log("Arguments found")
+//         } else {
+//         console.log("Argument found")
+//         }
+//     }
+// }
 
+
+
+
+
+const { argv } = require('node:process');
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
