9de3f3fd669f16c10f4e8782738afb9c9b30deae
hazem <hazemasaidui@gmail.com>
2025-09-14 15:10:52 +0300

    task 2 updated 3

--- 2-arguments.js
+++ 2-arguments.js
@@ -15,11 +15,12 @@
 
 
 const { argv } = require('node:process');
+const args = argv.slice(2);
 
-if (argv.length === 0) {
+if (args.length === 0) {
   console.log("No argument");
-} else if (argv.length > 1) {
-    console.log("Arguments found");
-} else {
+} else if (args.length === 1) {
     console.log("Argument found");
+} else {
+    console.log("Arguments found");
 }
